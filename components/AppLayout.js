cf590134bfc702aa4858dff75d7baaca9aec4043
Max Albrecht <1@178.is>
2021-04-07 22:23:49 +0200

    add link to source code repo

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,4 +1,6 @@
-const AppLayout = ({ children }) => {
+import React, { Fragment as F } from "react"
+
+const AppLayout = ({ menu, children }) => {
   return (
     <div className="app">
       {false && (
@@ -283,44 +285,20 @@ const AppLayout = ({ children }) => {
             </nav>
           )}
           <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            <div className="chartjs-size-monitor">
-              <div className="chartjs-size-monitor-expand"></div>
-              <div className="chartjs-size-monitor-shrink"></div>
-            </div>
             <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
               <h1 className="h3">
                 <b>audioplotter</b>
               </h1>
-              {false && (
+
+              {menu && (
                 <div className="btn-toolbar mb-2 mb-md-0">
-                  <div className="btn-group me-2">
-                    <button type="button" className="btn btn-sm btn-outline-secondary">
-                      Share
-                    </button>
-                    <button type="button" className="btn btn-sm btn-outline-secondary">
-                      Export
-                    </button>
-                  </div>
-                  <button type="button" className="btn btn-sm btn-outline-secondary dropdown-toggle">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width={24}
-                      height={24}
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      className="feather feather-calendar"
-                    >
-                      <rect x={3} y={4} width={18} height={18} rx={2} ry={2} />
-                      <line x1={16} y1={2} x2={16} y2={6} />
-                      <line x1={8} y1={2} x2={8} y2={6} />
-                      <line x1={3} y1={10} x2={21} y2={10} />
-                    </svg>
-                    This week
-                  </button>
+                  {menu.map((items, i) => (
+                    <div key={i} className="btn-group me-2">
+                      {items.map((m, i) => (
+                        <F key={i}>{m}</F>
+                      ))}
+                    </div>
+                  ))}
                 </div>
               )}
             </div>
