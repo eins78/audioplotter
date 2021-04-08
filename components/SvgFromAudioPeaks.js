3427ab1822af5f716ebff38d68dc0ffd7514978b
Max Albrecht <1@178.is>
2021-04-08 09:19:53 +0200

    add vertical padding
    angles might be cut off if the points are right on the border,
    add 10% padding

--- components/SvgFromAudioPeaks.js
+++ components/SvgFromAudioPeaks.js
@@ -3,6 +3,8 @@ import React from 'react'
 import { Svg, Polyline } from 'react-svg-path'
 
 export const STYLES = ['zigzag', 'saw']
+export const DEFAULT_WIDTH = 1000
+export const DEFAULT_PADDING_X = 100
 
 export default React.forwardRef(function SvgFromAudioPeaks(
   {
@@ -17,10 +19,13 @@ export default React.forwardRef(function SvgFromAudioPeaks(
 ) {
   if (!height) throw new TypeError()
 
-  const targetWidth = 1000
+  const targetWidth = DEFAULT_WIDTH
+  const paddingX = DEFAULT_PADDING_X
+  const targetHeight = parseInt(height, 10)
+
   const totalWidth = peaks.length + (withCaps ? 2 : 0)
   const distanceX = targetWidth / totalWidth
-  const middleY = height / 2
+  const middleY = targetHeight / 2
   const startPos = [0, middleY]
   const endPos = [targetWidth, middleY]
   let points = []
@@ -31,7 +36,7 @@ export default React.forwardRef(function SvgFromAudioPeaks(
       const isEven = index % 2 === 0
       const isUp = totalWidth % 2 === 0 ? isEven : !isEven // start up or down according to total width - less flicker when changing values?
       const xPos = index * distanceX + (withCaps ? distanceX : 0)
-      const distance = peak * height
+      const distance = peak * targetHeight
       const yPos = isUp ? middleY + distance : middleY - distance
       return [xPos, yPos]
     })
@@ -40,7 +45,7 @@ export default React.forwardRef(function SvgFromAudioPeaks(
   if (style === 'saw') {
     points = peaks.reduce((result, peak, index) => {
       const xPos = index * distanceX + (withCaps ? distanceX : 0)
-      const distance = peak * height
+      const distance = peak * targetHeight
       const yUp = middleY - distance
       const yDown = middleY + distance
       return result.concat([
@@ -55,7 +60,13 @@ export default React.forwardRef(function SvgFromAudioPeaks(
   }
 
   return (
-    <svg ref={ref} width={targetWidth} height={height} viewBox={`0 0 ${targetWidth} ${height}`} {...restProps}>
+    <svg
+      ref={ref}
+      width={targetWidth}
+      height={targetHeight}
+      viewBox={[0, -Math.floor(paddingX / 2), targetWidth, Math.floor(targetHeight + paddingX)].join(' ')}
+      {...restProps}
+    >
       <Polyline points={points} stroke="#222" strokeWidth={strokeWidth} fill="#ffffff" />
     </svg>
   )
