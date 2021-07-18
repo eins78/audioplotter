89fd83cc9903f5192ed04fabd5e3bd57813ba5b5
Max Albrecht <1@178.is>
2021-07-18 23:12:41 +0200

    add more icons etc from realfavicongenerator.net

--- pages/_app.js
+++ pages/_app.js
@@ -4,16 +4,28 @@ import '../styles/style.scss'
 
 const SITE_TITLE = 'audioplotter'
 
-// from https://github.com/vercel/next.js/blob/736db423528e66d3d8f7aa1174a3b5310d2a57a9/examples/progressive-web-app/pages/_app.js
-function pwaHead(params) {
+// from favicons package
+function faviconsHead() {
   return (
-    <Head>
+    <>
+      <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+      <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#500cbd" />
+      <meta name="apple-mobile-web-app-title" content="audiplotter" />
+      <meta name="application-name" content="audiplotter" />
+      <meta name="msapplication-TileColor" content="#500cbd" />
+      <meta name="theme-color" content="#500cbd" />
+    </>
+  )
+}
+
+// from https://github.com/vercel/next.js/blob/736db423528e66d3d8f7aa1174a3b5310d2a57a9/examples/progressive-web-app/pages/_app.js
+function pwaHead() {
+  return (
+    <>
       <link rel="manifest" href="/manifest.json" />
-      <link href="/icons/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
-      <link href="/icons/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
-      <link rel="apple-touch-icon" href="/apple-icon.png"></link>
-      <meta name="theme-color" content="#317EFB" />
-    </Head>
+    </>
   )
 }
 
@@ -30,7 +42,8 @@ function MyApp({ Component, pageProps }) {
         <title>{SITE_TITLE}</title>
         {/* <meta name="description" content="Description" /> */}
         {/* <meta name="keywords" content="Keywords" /> */}
-        {pwaHead}
+        {pwaHead()}
+        {faviconsHead()}
       </Head>
       <Component {...pageProps} />
     </>
