2a15052dca3d9bb9fa3ca0084584c61ceadb923f
Max Albrecht <1@178.is>
2021-07-18 02:18:44 +0200

    setup pwa from next.js example

---
+++ pages/_offline.js
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div className="container mt-4">
+      <h1>error: offline</h1>
+      <p>sorry, something went wrong and the app could not be loaded.</p>
+    </div>
+  )
+}
