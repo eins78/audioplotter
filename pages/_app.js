6e830538623a2d0b8d1806e3470c9a51c34dca8f
Max Albrecht <1@178.is>
2021-07-18 02:18:44 +0200

    setup pwa from next.js example

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,40 @@
+import Head from 'next/head'
+
 import '../styles/style.scss'
 
+const SITE_TITLE = 'audioplotter'
+
+// from https://github.com/vercel/next.js/blob/736db423528e66d3d8f7aa1174a3b5310d2a57a9/examples/progressive-web-app/pages/_app.js
+function pwaHead(params) {
+  return (
+    <Head>
+      <link rel="manifest" href="/manifest.json" />
+      <link href="/icons/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
+      <link href="/icons/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
+      <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+      <meta name="theme-color" content="#317EFB" />
+    </Head>
+  )
+}
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <title>{SITE_TITLE}</title>
+        {/* <meta name="description" content="Description" /> */}
+        {/* <meta name="keywords" content="Keywords" /> */}
+        {pwaHead}
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
